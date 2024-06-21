8740a28fb76b73043eeae23d1d70e448758570fa
ritik <flipit001@gmail.com>
2024-06-20 22:25:23 -0700

    removed server folder, added template server.py code.

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 """
 Structure of local server host
-Server
-    - storage/
+Storage
+    - files/
         - any files or folders used for storage
     
     - tmp/
@@ -15,9 +15,9 @@ __all__ = [
 ]
 
 # uses tar.xz format
-def encode(file_or_directory_path: str, to_path: str="./"):
+def encode(file_or_directory_path: str, to_path: str="./") -> None:
     shutil.make_archive(file_or_directory_path, "xztar", to_path, file_or_directory_path)
 
 
-def decode(file_path: str, to_path: str="./"):
+def decode(file_path: str, to_path: str="./") -> None:
     shutil.unpack_archive(filename=file_path, extract_dir=to_path, format="xztar")
