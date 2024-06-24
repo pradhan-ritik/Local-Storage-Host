969c61e7667d7f7dcc41617b6c5756f3952c999f
ritik <flipit001@gmail.com>
2024-06-23 21:58:26 -0700

    made ability to make folders.

--- api.py
+++ api.py
@@ -9,6 +9,7 @@ Storage
 """
 import shutil
 import os
+import tarfile
 
 _REMOVE_TAR_XZ = -len(".tar.xz")
 
@@ -20,10 +21,24 @@ def encode(file_or_directory_path: str, to_path: str="./") -> None:
 def decode(file_path: str, to_path: str="./") -> None:
     shutil.unpack_archive(filename=file_path, extract_dir=to_path, format="xztar")
 
+def encode_for_user(file_or_directory_path: str, to_path: str) -> str:
+    if os.path.isfile(file_or_directory_path):
+        return file_or_directory_path
+    
+    shutil.make_archive(file_or_directory_path, "zip", to_path, file_or_directory_path)
+    return f"{file_or_directory_path}.zip"
+
+def get_files(directory: str) -> list[str]:
+    with tarfile.open(directory, "r:xz") as fh:
+        files = fh.getmembers()
+
+    return [i.uname for i in files]
+
 def tar_xz_path_to_normal(filename: str) -> str:
     if filename.endswith(".tar.xz"):
         return filename[:_REMOVE_TAR_XZ]
-
+        
+    return filename
 def remove(path: str) -> None:
     if os.path.isfile(path):
         os.remove(path)
--- server.py
+++ server.py
@@ -1,21 +1,26 @@
 import os
 from flask import Flask, render_template, request, send_file, url_for
-from werkzeug.datastructures import FileStorage
 import api
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html", files=map(api.tar_xz_path_to_normal, os.listdir("files/")))
+    files, folders = [], []
+    for i in os.listdir("files"):
+        if i.endswith(".tar.xz"):
+            files.append(i)
+        else:
+            folders.append(i)
+    return render_template("index.html", path="", files=files, folders=folders)
 
-@app.route('/upload_files', methods=('POST',))
-def upload_folder():
+@app.route('/upload_files/', methods=('POST',))
+def upload_files_root():
     uploaded = ""
 
     data = request.files.getlist("files")
     for file_ in data:
-        path = f"files/{file_.filename}"
+        path = os.path.join("files/", file_.filename)
         data = file_.save(path)
         api.encode(path)
         api.remove(path)
@@ -23,12 +28,26 @@ def upload_folder():
 
     return render_template("upload_files/index.html", uploaded=uploaded)
 
-@app.route('/download/<filename>')
-def download(filename: str):
-    real_path = f"files/{filename}.tar.xz"
-    download_path = f"tmp/"
+@app.route('/make_folder/', methods=('POST',))
+def make_folder_root():
+    folder_name = f"files/{request.form.get('folder_name')}"
+    if os.path.exists(folder_name):
+        return render_template("make_folder/index.html", folder_name="ERROR: FILE ALREADY EXISTS")
+    os.mkdir(folder_name)
+    return render_template("make_folder/index.html", folder_name=folder_name)
+
+@app.route('/download/<path>')
+def download(path: str):
+    real_path = f"files/{path}.tar.xz"
+    download_path = f"tmp"
+    path_to = f"{download_path}/files/{path}"
     api.decode(real_path, download_path)
-    return send_file(f"{download_path}/files/{filename}", as_attachment=True)
+    send_path = api.encode_for_user(path_to, f"{download_path}/files")
+
+    if send_path != path_to:
+        api.remove(path_to)
+
+    return send_file(send_path, as_attachment=True)
 
 
 if __name__ == "__main__":
